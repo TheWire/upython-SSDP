b56c9ce822c2b4ea42f0fa16c0ba16cd73e160ae
TheWire <TheWire@users.noreply.github.com>
2023-01-22 00:48:20 +0000

    python library

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="upython_ssdp",
+    packages=find_packages(include=["upython_ssdp"]),
+    version="1.0.0",
+    description="simple service detection library",
+    author="Andrew Barber",
+    license="MIT",
+    install_requires=[],
+)
